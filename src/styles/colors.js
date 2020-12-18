6bdddec4072cada834792c927fd92bde9f2c4e18
Sibasish Mohanty <smsibasish@gmail.com>
2020-12-18 19:49:08 +0530

    opiniated folder structure

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export const bg = '#fafaf9';
+export const bgOffset = '#e7e5e4';
+export const text = '#1c1917';
+export const textOffset = '#404040';
+export const border = '#737373';
+export const primary = '#059669';
+export const primaryOffset = '#34d399';
+export const secondary = '#d946ef';
---
+++ src/styles/media-queries.js
@@ -0,0 +1,3 @@
+export const xl = '@media (min-width: 1408px)';
+export const lg = '@media (min-width: 1024px)';
+export const sm = '@media (min-width: 720px)';
