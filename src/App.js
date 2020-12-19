31c5bfc19c7860c2e7517a0ec0e86bc8c9219bd4
Sibasish Mohanty <smsibasish@gmail.com>
2020-12-19 15:54:06 +0530

    providers working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import * as React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, useTheme } from '@emotion/react';
 
 function App() {
+	const theme = useTheme();
 	return (
-		<>
+		<div
+			css={{
+				minHeight: '100vh',
+				background: theme.bg,
+				color: theme.text,
+			}}
+		>
 			<header>I am a header</header>
 			<main>
 				<h1>Hello World</h1>
 			</main>
 			<footer>I am a footer</footer>
-		</>
+		</div>
 	);
 }
 
